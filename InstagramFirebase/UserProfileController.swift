1f72495b4183c1e7dfce41cbe89c87de7067ba86
Stephan Wegmann <veterinarius@gmx.net>
2018-02-04 18:17:31 +0100

    InstagramFirebase_25

--- InstagramFirebase/UserProfileController.swift
+++ InstagramFirebase/UserProfileController.swift
@@ -13,30 +13,26 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
     
     let cellId = "cellId"
     
+    var userId: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .white
-        
-        navigationItem.title = FIRAuth.auth()?.currentUser?.uid
-        
-        fetchUser()
         
         collectionView?.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headerId")
         
         collectionView?.register(UserProfilePhotoCell.self, forCellWithReuseIdentifier: cellId)
         
         setupLogOutButton()
-        
-//        fetchPosts()
-        
-        fetchOrderedPosts()
+        fetchUser()
+//        fetchOrderedPosts()
     }
     
     var posts = [Post]()
     
     fileprivate func fetchOrderedPosts() {
-        guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+        guard let uid = self.user?.uid else { return }
         let ref = FIRDatabase.database().reference().child("posts").child(uid)
         
         ref.queryOrdered(byChild: "creationDate").observe(.childAdded, with: { (snapshot) in
@@ -123,13 +119,18 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
 
     var user: User?
     fileprivate func fetchUser() {
-        guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+        
+        let uid = userId ?? (FIRAuth.auth()?.currentUser?.uid ?? "")
+        
+        //guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
         
         FIRDatabase.fetchUserWithUID(uid: uid) { (user) in
             self.user = user
             
             self.navigationItem.title = self.user?.username
             self.collectionView?.reloadData()
+            
+            self.fetchOrderedPosts()
         }
     }
 }
